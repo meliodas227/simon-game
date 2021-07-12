b6efd05e6d1a213df75bf4d4685802e979203add
yousef <67870537+meliodas227@users.noreply.github.com>
2021-07-12 17:54:49 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -35,8 +35,16 @@ playing("#yellow", "sounds/yellow.mp3");
             nextSequence();
           }, 1000);
       }
-  })
-
+  });
+  $("body").on({'touchstart',function(){
+    if(!started){
+      $("h1").text("round "+level);
+      started = true;
+      setTimeout(function () {
+            nextSequence();
+          }, 1000);
+      }
+  });
 
 
 $(".btn").click(function(){
